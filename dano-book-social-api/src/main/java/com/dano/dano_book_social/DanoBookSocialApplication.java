5736b730f1b6915b49419b89552674427a8ca2b4
Ayoub Daoui <ayoubda18@gmail.com>
2025-02-12 22:50:48 +0100

    Backend config

---
+++ dano-book-social-api/src/main/java/com/dano/dano_book_social/DanoBookSocialApplication.java
@@ -0,0 +1,13 @@
+package com.dano.dano_book_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanoBookSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanoBookSocialApplication.class, args);
+	}
+
+}
---
+++ dano-book-social-api/src/test/java/com/dano/dano_book_social/DanoBookSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dano.dano_book_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanoBookSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
